2b6c364db3ef63bbdf2d4c7236e298b661ed8f70
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-08-24 22:33:56 +0300

    hbs

---
+++ src/fetchCountries.js
@@ -0,0 +1,4 @@
+// fetch('https://restcountries.eu/rest/v2/name/{name}')
+// .then(response => {
+//     return response.json();
+// })
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+
+fetch('https://restcountries.eu/rest/v2/name/ukraine')
+.then(response => {
+    return response.json();
+})
+.then(countrie => {
+    console.log(countrie)
+})
+.catch(error => {
+    console.log(error)
+})
