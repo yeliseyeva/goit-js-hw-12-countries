6987ce183a221d8fe26b90c26617bfd4049c7aac
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-08-25 14:04:17 +0300

    function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import './sass/main.scss';
+import countryCardTpl from '../src/partials/country-card.hbs';
+import countryListTpl from '../src/partials/countries.hbs';
 
-fetch('https://restcountries.eu/rest/v2/name/ukraine')
-.then(response => {
+const refs = {
+  input: document.querySelector('.input'),
+  countriesList: document.querySelector('.countries-list'),
+  countryCard: document.querySelector('.country-card'),
+};
+
+fetchCountry()
+  .then(renderCountyCard)
+  .catch(error => {
+    console.log(error);
+  });
+
+function fetchCountry() {
+  return fetch('https://restcountries.eu/rest/v2/name/').then(response => {
     return response.json();
-})
-.then(countrie => {
-    console.log(countrie)
-})
-.catch(error => {
-    console.log(error)
-})
+  });
+}
+
+function renderCountyCard(country) {
+  const card = countryCardTpl(country);
+  refs.countryCard.innerHTML = card;
+}
